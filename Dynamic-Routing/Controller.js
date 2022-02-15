0759729bb0237201913b1a7370830fac70df7002
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2022-02-16 00:21:35 +0530

    Update Controller.js

--- Dynamic-Routing/Controller.js
+++ Dynamic-Routing/Controller.js
@@ -2,6 +2,12 @@ exports.getProducts = (req,res,next) =>{
 ... do something
 }
 
-exports.deleteProduct = (req,res,next){
+exports.deleteProduct = (req,res,next)=>{
 ... do something
 }
+
+exports.editProducts = (req,res,next) =>{
+  ... do something
+}
+
+//refer to this file to see how controllers work :  https://github.com/mehul2015/JS-Backend-Snippets/blob/master/Model%2CRoutes%2CControllers/Controller.js
