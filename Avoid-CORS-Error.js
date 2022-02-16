118c56426200aa812554bad72712bbc617f7d743
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2022-02-16 15:57:35 +0530

    Create Avoid-CORS-Error.js

---
+++ Avoid-CORS-Error.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methdds", "GET, OST, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authentication");
+  next();
+});
+
+
+app.listen(8080);
