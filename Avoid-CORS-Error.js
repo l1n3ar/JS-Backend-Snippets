9777a231ca389278e2e2ed82da8d5dc4ab82cb3b
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2022-02-16 15:58:42 +0530

    Update Avoid-CORS-Error.js

--- Avoid-CORS-Error.js
+++ Avoid-CORS-Error.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
-
-
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
