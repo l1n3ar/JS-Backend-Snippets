afb33fb98942b0d6411568165a084f3c5a048fe1
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2022-03-07 23:23:31 +0530

    Create O-Auth-Setup.js

---
+++ O-Auth-Setup.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/keys');
+
+const app = express();
+
+passport.use(
+  new googleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
