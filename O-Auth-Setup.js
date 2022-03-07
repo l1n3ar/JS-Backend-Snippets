d150ec55b531ce031eae1bf14cf5225f4898f556
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2022-03-08 00:53:39 +0530

    Update O-Auth-Setup.js

--- O-Auth-Setup.js
+++ O-Auth-Setup.js
@@ -28,6 +28,8 @@ app.get(
   })
 );
 
+app.get("/auth/google/callback, passport.authenticate("google"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
