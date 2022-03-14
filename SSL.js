626dfa941210ad670e26f7c16ee5ce48a2dd56a0
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2022-03-14 16:07:45 +0530

    Update SSL.js

--- SSL.js
+++ SSL.js
@@ -30,3 +30,7 @@ mongoose
       });
   })
   .catch((error) => console.log(error));
+
+
+//In Terminal : openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
+//Common Name is mandatory field after you run this command otherwise certificate wont be issued
